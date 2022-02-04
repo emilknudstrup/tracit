832d403a70234744ea729c0965d691e0f1cd7c5f
Emil Knudstrup <emil@phys.au.dk>
2022-02-04 07:57:29 +0000

    Edited conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -13,8 +13,8 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath("../basta"))
-sys.path.insert(0, os.path.abspath("../examples"))
+sys.path.insert(0, os.path.abspath('../trace'))
+#sys.path.insert(0, os.path.abspath("../examples"))
 
 
 # -- Project information -----------------------------------------------------
