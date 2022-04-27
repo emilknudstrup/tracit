0756265fc61ad3b8200d1bd3b811d734eed669ea
Emil Knudstrup <emil@phys.au.dk>
2022-04-27 10:12:49 +0200

    path

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,9 +12,9 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('./../..'))
-sys.path.insert(0, os.path.abspath('./../../src/'))
-sys.path.insert(0, os.path.abspath('./../../src/tracit'))
+#sys.path.insert(0, os.path.abspath('./../..'))
+sys.path.insert(0, os.path.abspath('./../../src'))
+#sys.path.insert(0, os.path.abspath('./../../src/tracit'))
 
 
 # -- Project information -----------------------------------------------------
